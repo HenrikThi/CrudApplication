a6b32ba8cdfe236beb7e271cbbf99d69d5c49775
Henrik Thiess <34238591+HenrikThi@users.noreply.github.com>
2021-11-18 16:55:58 +0100

    init

---
+++ app.js
@@ -0,0 +1,97 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv/config");
+
+// ℹ️ Connects to the database
+require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require("hbs");
+
+const app = express();
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
+
+// default value for title local
+const projectName = "CrudApplication";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
+
+app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+
+// session configuration
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const DB_URL = process.env.MONGO;
+
+console.log(process.env.MONGO);
+console.log(process.env.SESSION_SECRET);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    // for how long is the user logged in -> this would be one day
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: DB_URL,
+    }),
+  })
+);
+
+// passport config
+const User = require("./models/User.model");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((userFromDB) => {
+      done(null, userFromDB);
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+// register the local strategy (login with username and password)
+passport.use(
+	new LocalStrategy((username, password, done) => {
+		// this logic will be executed when we log in
+		User.findOne({ username: username })
+			.then(userFromDB => {
+				if (userFromDB === null) {
+					// there is no user with this username
+					done(null, false, { message: 'Wrong Credentials' });
+				} else {
+					done(null, userFromDB);
+				}
+			})
+	})
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 👇 Start handling routes here
+const index = require("./routes/index");
+app.use("/", index);
+
+const auth = require("./routes/auth");
+app.use("/", auth);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const User = require("../models/User.model");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+// const ensureLogin = require("connect-ensure-login");
+
+router.get("/private-page", (req, res) => {
+  // router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", { user: req.user });
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/signup", (req, res, next) => {
+  const { username, password } = req.body;
+  if (username.length === 0 || password.length == 0) {
+    // if yes show the form again with a message
+    res.render("auth/signup", {
+      message: "Your username or password cannot be empty",
+    });
+    return;
+  }
+
+  User.findOne({ username: username }).then((userFromDB) => {
+    // if user exists
+    if (userFromDB !== null) {
+      // we render signup again
+      res.render("auth/signup", { message: "This username is already taken" });
+      return;
+    } else {
+      // if we reach this point this username can be used
+      // we hash the password and create the user in the database
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+
+      User.create({ username: username, password: hash })
+        .then((createdUser) => {
+          console.log(createdUser);
+          res.redirect("/login");
+          //TODO: does not work yet ;(
+          // log the user in using passport
+          // using basic-auth -> req.session.user
+          // req.login(createdUser, (err) => {
+          //   if (err) {
+          //     next(err);
+          //   } else {
+          //     res.redirect("/");
+          //   }
+          // });
+        })
+        .catch((err) => {
+          next(err);
+        });
+    }
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+    passReqToCallback: true,
+  })
+);
+
+module.exports = router;
