03cd27d6112c0f44ba7f426d0dc97411c1cb84a8
Henrik Thiess <thiess.henrik@web.de>
2021-11-22 13:34:53 +0100

    Adds styling for the galleries overview page.

--- routes/collection.js
+++ routes/collection.js
@@ -7,15 +7,27 @@ const Nft = require("../models/Nft.model");
 router.get("/collections", async (req, res, next) => {
   const users = await User.find();
 
+  users.forEach(
+    (user) =>
+      (user.lastChange = user.updatedAt
+        .toString()
+        .split(" ")
+        .slice(0, 5)
+        .join(" "))
+  );
+
   res.render("collection/overview", { users });
 });
 
 router.get("/collections/:id", async (req, res, next) => {
   const user = await User.findById(req.params.id).populate("nfts");
   const isOwner = req.user.id === req.params.id;
-  console.log(isOwner)
+  console.log(isOwner);
 
-  user.nfts.forEach(nft => nft.collectionDescription = nft.collectionDescription?.slice(0, 160))
+  user.nfts.forEach(
+    (nft) =>
+      (nft.collectionDescription = nft.collectionDescription?.slice(0, 160))
+  );
 
   res.render("collection/collection", { user, isOwner });
 });
